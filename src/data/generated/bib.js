5b7735de35ceb1faeea5e5170c22629b031a7104
alyzl93 <zimo20700690@gmail.com>
2025-05-19 23:07:38 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,93 @@
+﻿define({ entries : {
+    "cubuk2019autoaugment": {
+        "author": "Cubuk, E. D. and Zoph, B. and Shlens, J. and Le, Q. V.",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "doi": "10.1109/CVPR.2019.00312",
+        "pages": "113--123",
+        "title": "AutoAugment: Learning augmentation policies from data",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "diligenti2017semantic": {
+        "author": "Diligenti, M. and Gori, M. and Sacc{\\`a C.",
+        "doi": "10.1016/j.artint.2015.08.011",
+        "journal": "Artificial Intelligence",
+        "pages": "143--165",
+        "title": "Semantic-based regularization for learning and inference",
+        "type": "article",
+        "volume": "244",
+        "year": "2017"
+    },
+    "lacan2023gan": {
+        "author": "Lacan, A. and Sebag, M. and Hanczar, B.",
+        "doi": "10.1093/bioinformatics/btad239",
+        "journal": "Bioinformatics",
+        "number": "Suppl. 1",
+        "pages": "i111--i120",
+        "title": "GAN-based data augmentation for transcriptomics: Survey and comparative assessment",
+        "type": "article",
+        "volume": "39",
+        "year": "2023"
+    },
+    "liang2023logic": {
+        "author": "Liang, C. and Wang, W. and Miao, J. and Yang, Y.",
+        "doi": "10.48550/arXiv.2308.12595",
+        "journal": "arXiv preprint arXiv:2308.12595",
+        "title": "Logic-induced diagnostic reasoning for semi-supervised semantic segmentation",
+        "type": "article",
+        "year": "2023"
+    },
+    "sun2022prior": {
+        "author": "Sun, T. and Lu, C. and Ling, H.",
+        "doi": "10.48550/arXiv.2207.08877",
+        "journal": "arXiv preprint arXiv:2207.08877",
+        "title": "Prior knowledge guided unsupervised domain adaptation",
+        "type": "article",
+        "year": "2022"
+    },
+    "teshima2021incorporating": {
+        "author": "Teshima, T. and Sugiyama, M.",
+        "doi": "10.48550/arXiv.2103.00136",
+        "journal": "arXiv preprint arXiv:2103.00136",
+        "title": "Incorporating causal graphical prior knowledge via simple data augmentation",
+        "type": "article",
+        "year": "2021"
+    },
+    "von2021informed": {
+        "author": "Von Rueden, L. and Mayer, S. and Beckh, K. and Georgiev, B. and Giesselbach, S. and Heese, R. and Bringmann, O.",
+        "doi": "10.1109/TKDE.2021.3079836",
+        "journal": "IEEE Transactions on Knowledge and Data Engineering",
+        "title": "Informed machine learning\u2014a taxonomy and survey of integrating prior knowledge into learning systems",
+        "type": "article",
+        "year": "2021"
+    },
+    "winkel2024simplex": {
+        "author": "Winkel, D. and Strau\u00dft, N. and Schubert, M. and Seidl, T.",
+        "doi": "10.48550/arXiv.2404.10683",
+        "journal": "arXiv preprint arXiv:2404.10683",
+        "title": "Simplex decomposition for portfolio allocation constraints in reinforcement learning",
+        "type": "article",
+        "year": "2024"
+    },
+    "xie2019embedding": {
+        "author": "Xie, Y. and Xu, Z. and Kankanhalli, M. S. and Meel, K. and Soh, H.",
+        "booktitle": "Advances in Neural Information Processing Systems",
+        "doi": "10.5555/3454287.3454668",
+        "pages": "1713--1728",
+        "title": "Embedding symbolic knowledge into deep networks",
+        "type": "inproceedings",
+        "volume": "32",
+        "year": "2019"
+    },
+    "xu2018semantic": {
+        "author": "Xu, J. and Zhang, Z. and Friedman, T. and Liang, Y. and Van den Broeck, G.",
+        "booktitle": "Proceedings of the 35th International Conference on Machine Learning",
+        "doi": "10.48550/arXiv.1711.11157",
+        "pages": "5502--5511",
+        "series": "Proceedings of Machine Learning Research",
+        "title": "A semantic loss function for deep learning with symbolic knowledge",
+        "type": "inproceedings",
+        "volume": "80",
+        "year": "2018"
+    }
+}});
